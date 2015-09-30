6cf15551b562065240ca08eb81d044cfb9f7387f
soulleaper <souldinnie1990@gmail.com>
2015-09-30 15:04:07 +0800

    Create inet.h

---
+++ inet.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <dirent.h>
+#include <signal.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#define SERV_TCP_PORT 25000
+#define SERV_UDP_PORT 35001
+#define CLI_UDP_PORT 35002
